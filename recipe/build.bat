e40d142027230b1d890edf1f6625232367b4f551
ClayWarren <warren.clay@icloud.com>
2026-09-12 04:13:55 -0500

    Enable native ARM64 checksums with upstream and installed CRC tests

--- recipe/build.bat
+++ recipe/build.bat
@@ -7,8 +7,13 @@ cmake -G "Ninja" ^
       -DCMAKE_INSTALL_LIBDIR=lib ^
       -DCMAKE_BUILD_TYPE=Release ^
       -DBUILD_SHARED_LIBS=ON ^
+      -DBUILD_TESTING=ON ^
       ..
 if errorlevel 1 exit 1
 
 ninja install
+if errorlevel 1 exit 1
+
+set "PATH=%CD%;%LIBRARY_BIN%;%PATH%"
+ctest --output-on-failure -C Release
 if errorlevel 1 exit 1
